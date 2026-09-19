b73e25f4eb66704fea5b0eb65476725b116e6a6e
hashfi <hashfi@users.noreply.github.com>
2026-09-19 14:46:49 +0700

    fix: add missing hashlib import

--- contracts/p2p_escrow_studio.py
+++ contracts/p2p_escrow_studio.py
@@ -1,6 +1,7 @@
 # { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
 from genlayer import *
 import json
+import hashlib
 
 class P2PEscrow(gl.Contract):
     trades : TreeMap[u256, str]
